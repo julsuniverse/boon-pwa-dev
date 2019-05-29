380ad1751a625ac7124be553acc72a71daa9dc69
Admin <admin@Ganzin_test_mac-mini.lan>
2019-05-29 10:21:53 +0300

    builded app

---
+++ OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Admin on 5/28/19.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
